e812bf8d153d1d142ca1484da751d66797ee8d26
ThopsD <74445064+thops1925@users.noreply.github.com>
2022-08-26 04:10:57 +0800

    stock

--- src/App.js
+++ src/App.js
@@ -6,13 +6,6 @@ function App() {
   return (
     <div className='App'>
       <Navbar />
-
-      <div className='shadow_container'>
-        <div className='left_shadow' />
-        <div className='right_shadow' />
-      </div>
-
-
       <Home />
       <Discover />
       <About />
